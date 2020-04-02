a54a4539580fe62676cf7a4e08adc2fe04480b0e
Joseph Kao <josephtkao@gmail.com>
2020-04-02 21:13:11 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,25 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # top row
+  [3,4,5], # middle row
+  [6,7,8], # bottom row
+  [0,3,6], # left columns
+  [1,4,7], # middle columns
+  [2,5,8], # right columns
+  [0,4,8], # diagonals
+  [2,4,6] #diagonals
+]
+
+board = ["X", "X", "X","","","","","",""]
+def won?(board)
+  WIN_COMBINATIONS.each do |combination|
+    combination.all? do |tokens|
+      state = tokens == "X" || tokens == "O"
+    end
+  end
+  return "#{state}"
+end
+
+won?(board)
